6cb5b02445feb1175f35b6471d60f4f1d2a45972
HarshJa1n <hjain012003@gmail.com>
2022-08-24 18:30:20 +0530

    Made First Section

--- src/components/firstSection/index.js
+++ src/components/firstSection/index.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import Button from "../common/buttons";
+import "./firstSection.css";
 const FirstSection = () => {
-  return <div>This is FirstSection</div>;
+  return (
+    <div className="first-section-wrapper">
+      <div className="flex make-center flex-col max-width first-section">
+        <div className="first-heading">
+          rewards for paying credit card bills.
+        </div>
+        <div className="first-subheading">
+          join 9M+ members who win rewards and cashbacks everyday
+        </div>
+        <Button buttonText="Download CRED" />
+      </div>
+    </div>
+  );
 };
 
 export default FirstSection;
