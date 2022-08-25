c7d8919acd3f3426e525505340c3eb64101ce1dc
HarshJa1n <hjain012003@gmail.com>
2022-08-25 19:14:56 +0530

    claim now label done

--- src/components/firstSection/index.js
+++ src/components/firstSection/index.js
@@ -4,6 +4,17 @@ import "./firstSection.css";
 const FirstSection = () => {
   return (
     <div className="first-section-wrapper">
+      <div className="flex make-center claim-label">
+        <div> pay credit card bill. earn guaranteed ₹200 back.</div>
+        <div className="claim-anchor">
+          claim now
+          <img
+            src="https://web-images.credcdn.in/_next/assets/images/home-page/arrow.png"
+            alt="claim arrow png"
+            className="claim-anchor"
+          />
+        </div>
+      </div>
       <div className="flex make-center flex-col max-width first-section">
         <div className="first-heading">
           rewards for paying credit card bills.
