d22253c693bbcc7795feb338151e17a0e1d39dde
HarshJa1n <hjain012003@gmail.com>
2022-08-26 17:13:36 +0530

    header complete

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
+  const [onMenu, setOnMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOnMenu(!onMenu);
+  };
+
   return (
     <div className="menu-wrapper">
+      <div className={`mobile-menu only-mobile ${onMenu ? "overlay" : ""}`}>
+        <div className="mobile-navbar">
+          <div className="mobile-nav-item">credit score check</div>
+          <div className="mobile-nav-item">credit card bill payment</div>
+        </div>
+      </div>
       <div className="flex max-width header">
         <img
           src="https://web-images.credcdn.in/_next/assets/images/home-page/cred-logo.png"
           alt="cred logo"
           className="header-logo"
         />
+        <div className="only-mobile mobile-menu-button">
+          <button
+            class={`hamburger hamburger--spin ${onMenu ? "is-active" : ""}`}
+            type="button"
+            onClick={toggleMenu}
+          >
+            <span class="hamburger-box">
+              <span class="hamburger-inner"></span>
+            </span>
+          </button>
+        </div>
         <div className="not-mobile flex">
           <div className="nav-item">credit score check</div>
           <div className="nav-item">credit card bill payment</div>
