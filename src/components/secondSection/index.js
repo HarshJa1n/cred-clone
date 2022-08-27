522b128a9c1c38d4a0cd644bcc562d0118a9288c
HarshJa1n <hjain012003@gmail.com>
2022-08-27 22:28:52 +0530

    second section start

--- src/components/secondSection/index.js
+++ src/components/secondSection/index.js
@@ -1,7 +1,38 @@
 import React from "react";
+import "./secondSection.css";
 
 const SecondSection = () => {
-  return <div>SecondSection</div>;
+  return (
+    <div className="complete-section">
+      <div className="scimage-group">
+        <img
+          src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/left-2.png"
+          alt="image1"
+          className="scimage image-1"
+        />
+        <img
+          src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/left-1.png"
+          alt="image2"
+          className="scimage image-2"
+        />
+        <img
+          src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/center.png"
+          alt="image3"
+          className="scimage image-3"
+        />
+        <img
+          src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/right-1.png"
+          alt="image4"
+          className="scimage image-4"
+        />
+        <img
+          src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/right-2.png"
+          alt="image5"
+          className="scimage image-5"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SecondSection;
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import FirstSection from "../components/firstSection";
 import Header from "../components/common/header";
+import SecondSection from "../components/secondSection";
 
 const HomePage = () => {
   return (
     <>
       <Header />
-      <FirstSection />;
+      <FirstSection />
+      <SecondSection />
     </>
   );
 };
